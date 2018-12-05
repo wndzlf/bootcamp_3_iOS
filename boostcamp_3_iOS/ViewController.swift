ccb7c562dc7ea98f823022a5ffbec8ba3d05c0e7
wndzlf <wndzlf@gmail.com>
2018-12-05 19:16:20 +0900

    액션 시트 추가

--- boostcamp_3_iOS/ViewController.swift
+++ boostcamp_3_iOS/ViewController.swift
@@ -24,6 +24,37 @@ class ViewController: UIViewController {
         getJsonFromURL()
     }
     
+    @IBAction func flteringButton(_ sender: Any) {
+        print("asdgasdgasdg")
+        let actionSheet = UIAlertController(title: "정렬 방식 선택", message:"영화를 어떤 방식으로 정렬할까요?", preferredStyle: .actionSheet)
+        
+        let reservationRate = UIAlertAction(title: "예매율", style: .default) { [weak self] (action) in
+            
+            guard let `self` = self else {return}
+            
+            let sorted = self.movies.sorted(by: { $0.reservation_rate > $1.reservation_rate })
+            self.movies = sorted
+            self.tableview.reloadData()
+        }
+        let quaration = UIAlertAction(title: "큐레이션", style: .default) { (action) in
+            
+        }
+        let openTime = UIAlertAction(title: "개봉일", style: .default) { (action) in
+            
+        }
+        let cancle = UIAlertAction(title: "취소", style: .cancel) { (action) in
+            
+        }
+        
+        actionSheet.addAction(reservationRate)
+        actionSheet.addAction(quaration)
+        actionSheet.addAction(openTime)
+        actionSheet.addAction(cancle)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    
     func setupNavigation() {
         navigationItem.title = "예매율순"
         let barColor = UIColor(red:0.47, green:0.42, blue:0.91, alpha:1.0)
@@ -49,26 +80,21 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource, URLSessionDownloadDelegate {
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        debugPrint("Download finished: \(location)")
-    }
-    
-    
+extension ViewController: UITableViewDataSource{
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(movies.count)
         return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId") as! tableviewCell
         let movie = movies[indexPath.row]
+        
         //date는 formatt형식 바꾸기
         cell.movieDate.text = "개봉일: \(movie.date)"
-        cell.movieReserRate.text = "\(movie.reservation_rate)"
-        cell.movieGrade.text = "\(movie.reservation_grade)"
-        cell.movieRate.text = "\(movie.user_rating)"
+        cell.movieReserRate.text = "예매율:\(movie.reservation_rate)"
+        cell.movieGrade.text = "예매순위:\(movie.reservation_grade)"
+        cell.movieRate.text = "평점:\(movie.user_rating)"
         cell.movieTitle.text = movie.title
         
         //download in background
@@ -86,7 +112,7 @@ extension ViewController: UITableViewDelegate {
 }
 
 //https://www.hackingwithswift.com/example-code/uikit/how-to-load-a-remote-image-url-into-uiimageview
-//GCD에 대해 공부하자
+//GCD개념 숙지
 extension UIImageView {
     func load(url: URL) {
         DispatchQueue.global().async { [weak self] in
