cc5dd03e73ac6f41b2b16490dbfba187a1a3b0d3
GongUi <zhanggoni@gmail.com>
2019-01-20 15:19:48 +0900

    Cell 설정 관련 메소드 이동 리펙토링
    Cell 초기화 관련 코드를 VC -> Cell 로 이동

--- boostcamp_3_iOS/ViewController/CollectionView/CollectionViewCell.swift
+++ boostcamp_3_iOS/ViewController/CollectionView/CollectionViewCell.swift
@@ -17,4 +17,28 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet var movieAge: UIImageView!
     @IBOutlet var rate: UILabel!
     
+    public func setMovie(movie model: Movie, itemIdx idx: Int) {
+        
+        title.text = model.title
+        title.sizeToFit()
+        rating.text = "  / \(model.reservation_rate)%"
+        rating.sizeToFit()
+        date.text = model.date
+        date.sizeToFit()
+        rate.text = "\(idx + 1)위 (\(model.user_rating))"
+        rate.sizeToFit()
+        
+        if model.grade == 0 {
+            movieAge.image = UIImage(named:"all")
+        }else if model.grade == 12 {
+            movieAge.image = UIImage(named:"12")
+        }else if model.grade == 15 {
+            movieAge.image = UIImage(named:"15")
+        }else  {
+            movieAge.image = UIImage(named:"19")
+        }
+        
+        let imageURL = URL(string: model.thumb)!
+        poster.load(url: imageURL)
+    }
 }
